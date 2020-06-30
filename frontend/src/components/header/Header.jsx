3f7936329e620a971956d7b845367beedae8953c
Boyan Liu <lbyybl1996@gmail.com>
2020-06-30 18:30:16 -0400

    working on header style 1

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,31 +1,45 @@
 import React from "react";
-//point to home page
-import { Link } from "react-router-dom";
 //import icon
 import Logo from "../../Imgs/logo.png";
+//export styled Components;
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+  NavContainer,
+  OrderList,
+  UnorderList,
+} from "./Header.style.jsx";
+
 const Header = () => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <img src={Logo} alt="logo" className="Logo" />
-    </Link>
+  <HeaderContainer className="HeaderContainer">
+    <NavContainer>
+      <LogoContainer className="logo">
+        <img src={Logo} alt="logo" className="Logo" />
+      </LogoContainer>
 
-    {/* All the options will remain inside another div */}
-
-    <div className="options">
-      <Link className="option" to="/about">
-        ABOUT
-      </Link>
-      <Link className="option" to="/blog">
-        BLOG
-      </Link>
-      <Link className="option" to="/faq">
-        FAQ
-      </Link>
-      <Link className="option" to="/login">
-        Login
-      </Link>
-    </div>
-  </div>
+      {/* All the options will remain inside another div */}
+      <OptionsContainer className="LinkContainer">
+        <UnorderList>
+          <OrderList>
+            <OptionLink to="/about">ABOUT</OptionLink>
+          </OrderList>
+          <OrderList>
+            <OptionLink to="/blog">BLOG</OptionLink>
+          </OrderList>
+          <OrderList>
+            <OptionLink to="/faq">FAQ</OptionLink>
+          </OrderList>
+          <OrderList>
+            <OptionLink className="login" to="/login">
+              Login
+            </OptionLink>
+          </OrderList>
+        </UnorderList>
+      </OptionsContainer>
+    </NavContainer>
+  </HeaderContainer>
 );
 
 export default Header;
---
+++ frontend/src/components/header/Header.style.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const HeaderContainer = styled.div`
+  box-shadow: none;
+  padding-top: 20px;
+  color: #888;
+  background: #fff !important;
+  padding-bottom: 0.625rem;
+  min-height: 53px;
+  margin-bottom: 20px;
+  position: fixed;
+  right:0;
+  left:0;
+  z-index;1030;
+`;
+
+export const LogoContainer = styled(Link)``;
+
+export const OptionsContainer = styled.div``;
+
+export const OptionLink = styled(Link)``;
+export const UnorderList = styled.ul``;
+export const OrderList = styled.li``;
+export const NavContainer = styled.nav``;
