57165317b643ff3b7837e1d3a89c8e232e1c11c3
Boyan Liu <lbyybl1996@gmail.com>
2020-06-30 20:27:44 -0400

    add style on header

--- frontend/src/components/header/Header.style.jsx
+++ frontend/src/components/header/Header.style.jsx
@@ -15,11 +15,32 @@ export const HeaderContainer = styled.div`
   z-index;1030;
 `;
 
-export const LogoContainer = styled(Link)``;
+export const LogoContainer = styled(Link)`
+  display: block;
+  cursor: pointer;
+`;
 
 export const OptionsContainer = styled.div``;
-
-export const OptionLink = styled(Link)``;
-export const UnorderList = styled.ul``;
-export const OrderList = styled.li``;
-export const NavContainer = styled.nav``;
+// flex-direction: row;
+// margin-left: auto !important;
+export const OptionLink = styled(Link)`
+  font-size: 0.7142em;
+  padding: 0.5rem 0.7rem;
+  line-height: 1.625rem;
+`;
+export const UnorderList = styled.ul`
+  list-style: none;
+  margin-bottom: 0;
+  padding-left: 0px 20px;
+`;
+export const OrderList = styled.li`
+  text-transform: uppercase;
+  display: inline-block;
+  margin-right: 5px;
+`;
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  //push left and right item in the far edge, item in the //.//middle will even space in between
+  justify-content: space-between;
+`;
